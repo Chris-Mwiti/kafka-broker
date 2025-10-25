c9c64e377ce928e159ddb8e466d2b9588644345c
Chris-Mwiti <chrismwiti21@gmail.com>
2025-10-25 13:36:35 +0300

    changes: Configured tcp handlers logic

--- app/conn.go
+++ app/conn.go
@@ -92,40 +92,4 @@ func (c *Conn) HandleConn()(error){
 	}
 }
 
-//Responsible for handling kafka topic request
-func (c *Conn) HandleTopicConn()(error){
-	buff := new(bytes.Buffer)
-
-	for {
-		buff.Reset()
-		data := make([]byte, 100)
-		_, err := c.conn.Read(data)
-		buff.Write(data)
-		if err != nil {
-			log.Printf("error while receiving data from conn %v\n",err)
-			if err == io.EOF{
-				log.Println("EOF")
-				return ERR_EOF 
-			}
-			return err
-		}
-		response,err := c.handleTopicRequest(buff.Bytes())
-		log.Printf("handl topic request: %x\n", response)
-		if err != nil {
-			//@todo: Improve on the error handling logic
-			log.Printf("error while parsing response %v\n", err)
-			return err
-
-		}
-		_, err = c.conn.Write(response)
-		if err != nil {
-			log.Printf("error while writing to the connection: %v\n", err)
-			return err
-		}
-	}
-}
-
-
-
-
 
